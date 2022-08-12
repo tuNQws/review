90ebe09029282db227ce3e286e0b46ed36ebf629
tuNQws <91256783+tuNQws@users.noreply.github.com>
2022-08-12 10:04:40 +0700

    finish stack

---
+++ data structure/Stack/implementation.cpp
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node* pNext;
+};
+
+struct Stack
+{
+    Node* top;
+};
+
+Node* CreateNode(int data)
+{
+    Node* p = new Node();
+    p->data = data;
+    p->pNext = nullptr;
+    return p;
+}
+
+void CreateStack(Stack& s){
+    s.top = nullptr;
+}
+
+bool isEmpty(Stack s){
+    if (s.top == nullptr){
+        return true;
+    }
+    return false;
+}
+
+void Show(Stack s){
+    if (s.top != nullptr)
+    {
+        for (Node* i = s.top; i != nullptr; i = i->pNext){
+            cout << i->data;
+            if (i->pNext != nullptr){
+                cout << " --> ";
+            }
+        }
+        cout << endl;
+    }
+    else
+        cout << "Empty\n";
+}
+
+int random()
+{
+    srand(time(NULL));
+    int res = rand() % 100;
+    return res;
+}
+
+void enqueue(Node* p, Stack& s){
+    if (isEmpty(s)){
+        s.top = p;
+    } else{
+        Node* q = s.top;
+        p->pNext = q;
+        s.top = p;
+    }
+}
+
+void dequeue(Stack& s, Node*& p){
+    if (!isEmpty(s)){
+        p = s.top;
+        s.top = s.top->pNext;
+    }
+}
+
+void search(Stack s, int key, Node*& p){
+    for (Node* i = s.top; i != nullptr; i = i->pNext){
+        if (i->data == key){
+            cout << "Founded!\n";
+            p = i;
+            return;
+        }
+    }
+    cout << "Not founded!\n";
+}
+
+int main()
+{
+    int choice;
+    Stack s;
+    CreateStack(s);
+
+    while (true)
+    {
+        Show(s);
+        cout << "0. End \n";
+        cout << "1. Enqueue randomly \n";
+        cout << "2. Dequeue \n";
+        cout << "3. Search for \"a\" \n";
+        cout << " ====================== \n";
+        cout << "Your choice: ";
+        cin >> choice;
+        Node* p = new Node();
+        int a;
+
+        switch (choice)
+        {
+        case 0:
+            return 0;
+        case 1:
+            enqueue(CreateNode(random()), s);
+            break;
+        case 2:
+            dequeue(s, p);
+            break;
+        case 3:
+            cout<< "input a: ";
+            cin >> a;
+            search(s, a, p);
+            break;
+        default:
+            break;
+        }
+    }
+}
